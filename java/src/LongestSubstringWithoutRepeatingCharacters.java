cc5b6a2f0fcd534d115bce316ee0dd500d771e55
MichaelJamesParsons <mjay.parsons@gmail.com>
2017-07-15 15:01:22 -0500

    LongestSubstringWithoutRepeatingCharacters - Java

---
+++ java/src/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+
+class LongestSubstringWithoutRepeatingCharacters {
+    int lengthOfLongestSubstring(String s) {
+        int count;
+        int maxLength = 0;
+        HashSet<Character> usedLetters;
+
+        for (int i = 0; i < s.length(); i++) {
+            count = 0;
+            usedLetters = new HashSet<>();
+            for (int j = i; j < s.length(); j++) {
+                if (usedLetters.contains(s.charAt(j))) {
+                    break;
+                }
+
+                usedLetters.add(s.charAt(j));
+                count++;
+            }
+
+            if (count > maxLength) {
+                maxLength = count;
+            }
+        }
+
+        return maxLength;
+    }
+}
---
+++ java/tests/LongestSubstringWithoutRepeatingCharactersTests.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class LongestSubstringWithoutRepeatingCharactersTests {
+    private LongestSubstringWithoutRepeatingCharacters longestSubstr;
+
+    @BeforeEach
+    void init() {
+        this.longestSubstr = new LongestSubstringWithoutRepeatingCharacters();
+    }
+
+    @Test
+    void testExample1() {
+        Assertions.assertEquals(3, this.longestSubstr.lengthOfLongestSubstring("abcabcbb"));
+    }
+
+    @Test
+    void testExample2() {
+        Assertions.assertEquals(1, this.longestSubstr.lengthOfLongestSubstring("bbbbb"));
+    }
+
+    @Test
+    void testExample3() {
+        Assertions.assertEquals(3, this.longestSubstr.lengthOfLongestSubstring("pwwkew"));
+    }
+}
