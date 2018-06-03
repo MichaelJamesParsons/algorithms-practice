ccc9bc592a1b9da67633c6180311f8cd73f9eff3
Michael-James Parsons <mjay.parsons@gmail.com>
2018-06-03 15:20:35 -0400

    Cycle detection in linked list.

--- java/src/LinkedList/LinkedList.java
+++ java/src/LinkedList/LinkedList.java
@@ -1,19 +1,19 @@
 package LinkedList;
 
-class Node {
+class ListNode {
     public int val;
-    public Node next;
+    public ListNode next;
 
-    Node(int val) {
+    ListNode(int val) {
         this.val = val;
     }
 }
 
 public class LinkedList {
-    private Node head;
+    private ListNode head;
 
-    private Node getNode(int index) {
-        Node n = this.head;
+    private ListNode getNode(int index) {
+        ListNode n = this.head;
         int i = 0;
 
         if (index < 0) {
@@ -33,7 +33,7 @@ public class LinkedList {
     }
 
     public int get(int index) {
-        Node n = this.getNode(index);
+        ListNode n = this.getNode(index);
 
         if (n != null) {
             return n.val;
@@ -43,7 +43,7 @@ public class LinkedList {
     }
 
     public void addAtHead(int val) {
-        Node n = new Node(val);
+        ListNode n = new ListNode(val);
 
         if (this.head != null) {
             n.next = this.head;
@@ -53,10 +53,10 @@ public class LinkedList {
     }
 
     public void addAtTail(int val) {
-        Node n = this.head;
+        ListNode n = this.head;
 
         if (n == null) {
-            this.head = new Node(val);
+            this.head = new ListNode(val);
             return;
         }
 
@@ -64,11 +64,11 @@ public class LinkedList {
             n = n.next;
         }
 
-        n.next = new Node(val);
+        n.next = new ListNode(val);
     }
 
     public void addAtIndex(int index, int val) {
-        Node nodeToAdd = new Node(val);
+        ListNode nodeToAdd = new ListNode(val);
 
         if (index == 0) {
             if (this.head != null) {
@@ -79,7 +79,7 @@ public class LinkedList {
             return;
         }
 
-        Node n = this.getNode(index - 1);
+        ListNode n = this.getNode(index - 1);
 
         if (n != null) {
             nodeToAdd.next = n.next;
@@ -96,10 +96,35 @@ public class LinkedList {
             return;
         }
 
-        Node n = this.getNode(index - 1);
+        ListNode n = this.getNode(index - 1);
 
         if (n != null && n.next != null) {
             n.next = n.next.next;
         }
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        // If a cycle does not exist, fast will reach
+        // the end first.
+        while (fast != null && fast.next != null) {
+            // On first iteration, it's likely the pointers are
+            // in different positions, so we might as well move
+            // them before making comparisons.
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
