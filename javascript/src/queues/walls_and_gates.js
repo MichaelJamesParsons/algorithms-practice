8a19efbfa88e63037b0259be2af94493d00fa4b1
Michael-James Parsons <mjay.parsons@gmail.com>
2018-07-27 20:38:35 -0400

    Walls and gates

--- javascript/src/queues/walls_and_gates.js
+++ javascript/src/queues/walls_and_gates.js
@@ -17,7 +17,6 @@ var bfs = function (i, j, rooms) {
   ];
 
   var stack = [];
-  var visited = {};
   stack.push([i, j, 0]);
 
   while (stack.length > 0) {
@@ -27,17 +26,14 @@ var bfs = function (i, j, rooms) {
     for (var x = 0; x < moves.length; x++) {
       var row = cell[0] + moves[x][0];
       var col = cell[1] + moves[x][1];
-      var key = "" + row + col;
 
       if (row < rooms.length && row >= 0
           && col < rooms[0].length && col >= 0
           && rooms[row][col] !== -1
           && rooms[row][col] !== 0
-          && (rooms[row][col] === 2147483647 || rooms[row][col] > cell[2] + 1)
-          && !visited.hasOwnProperty(key)) {
+          && (rooms[row][col] === 2147483647 || rooms[row][col] > cell[2])) {
         stack.push([row, col, cell[2] + 1]);
-        visited[key] = true;
       }
     }
-  };
+  }
 };
