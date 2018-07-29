f067ce5a4cf46e18410b84a69937dfd851daf272
Michael-James Parsons <mjay.parsons@gmail.com>
2018-07-28 22:17:36 -0400

    Perfect squares

---
+++ javascript/src/queues/perfect_squares.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numSquares = function(n) {
+  var squares = [];
+  var x = 1;
+  while (x * x <= n) {
+    var square = x * x;
+    for (var i = square; i <= n; i += square) {
+      squares.push(square);
+    }
+
+    x++;
+  }
+
+  return knapsack(squares.length - 1, squares, n, 0);
+};
+
+var knapsack = function (index, squares, remaining, numSquares) {
+  if (remaining === 0) {
+    return numSquares;
+  } else if (index < 0) {
+    return Number.MAX_VALUE;
+  }
+
+  if (squares[index] > remaining) {
+    return knapsack(index - 1, squares, remaining, numSquares);
+  }
+
+  var take = knapsack(index - 1, squares, remaining - squares[index], numSquares + 1);
+  var skip = knapsack(index - 1, squares, remaining, numSquares);
+
+  return Math.min(take, skip);
+};
