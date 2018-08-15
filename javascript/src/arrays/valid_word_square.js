0cf0e72464c6d220f7cf01ab4f3d7c514fe8bcce
Michael-James Parsons <mjay.parsons@gmail.com>
2018-08-15 00:40:36 -0400

    Valid word square

---
+++ javascript/src/arrays/valid_word_square.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string[]} words
+ * @return {boolean}
+ */
+var validWordSquare = function(words) {
+  for (var x = 0; x < words.length; x++) {
+    var y = 0;
+    var wordLen = words[x].length;
+
+    while (y < x + 1) {
+      // row is longer than column OR
+      // column is longer than row OR
+      // word is longer than list OR
+      // characters are not the same.
+      if ((y >= words[x].length && x < words[y].length) ||
+        (y < words[x].length && x >= words[y].length) ||
+        words[x].length > words.length ||
+        words[x][y] !== words[y][x]) {
+        return false;
+      }
+
+      y++;
+    }
+  }
+
+  return true;
+};
